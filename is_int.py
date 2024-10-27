a3ed95aa4abc8a10a5899b00e96f29428c4772be
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:14:44 +0700

    Update is_int.py
    Добавлены проверки, что s число

--- is_int.py
+++ is_int.py
@@ -1,19 +1,23 @@
-def is_int(s):
+def is_int2(s):
     """
     Проверка на то, что s - целое число
     """
     try:
         int(s)
         return True
-    except:
+    except (Exception, ValueError, TypeError):
         return False
 
 
-def is_int2(s):
+def is_int3(s):
     """
     Проверка на то, что s - целое число
     """
     try:
+        if type(s) is int:
+            return True
+        if not s.isdecimal():
+            return False
         int(s)
         return True
     except (Exception, ValueError, TypeError):
@@ -39,15 +43,6 @@ if __name__ == "__main__":
 
 
     if True:
-        test("", is_int)
-        test(None, is_int)
-        test("1", is_int)
-        test(2, is_int)
-        test("0.3", is_int)
-        test("f", is_int)
-
-        print('')
-
         test("", is_int2)
         test(None, is_int2)
         test("1", is_int2)
@@ -55,19 +50,27 @@ if __name__ == "__main__":
         test("0.3", is_int2)
         test("f", is_int2)
 
-        """
-     ->False 18 ms (is_int)
- None->False 12 ms (is_int)
-    1->True 3 ms (is_int)
-    2->True 2 ms (is_int)
-  0.3->False 20 ms (is_int)
-    f->False 19 ms (is_int)
+        print('')
+        
+        test("", is_int3)
+        test(None, is_int3)
+        test("1", is_int3)
+        test(2, is_int3)
+        test("0.3", is_int3)
+        test("f", is_int3)
 
-     ->False 18 ms (is_int2)
- None->False 14 ms (is_int2)
+        """
+     ->False 16 ms (is_int2)
+ None->False 12 ms (is_int2)
     1->True 3 ms (is_int2)
     2->True 2 ms (is_int2)
   0.3->False 17 ms (is_int2)
-    f->False 17 ms (is_int2)
+    f->False 16 ms (is_int2)
+
+     ->False 2 ms (is_int3)
+ None->False 10 ms (is_int3)
+    1->True 4 ms (is_int3)
+    2->True 1 ms (is_int3)
+  0.3->False 1 ms (is_int3)
+    f->False 2 ms (is_int3)
         """
-        
