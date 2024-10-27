a128acc7a7101bc2e9f08353be9aaeb2c10cf90f
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:09:59 +0700

    Update is_int.py
    в is_int2 исправлено замечание к  except:
    Выявлены ошибки типа ValueError и TypeError. Но в данном примере смысла их использовать нет.

--- is_int.py
+++ is_int.py
@@ -9,30 +9,26 @@ def is_int(s):
         return False
 
 
+def is_int2(s):
+    """
+    Проверка на то, что s - целое число
+    """
+    try:
+        int(s)
+        return True
+    except (Exception, ValueError, TypeError):
+        return False
+
+
 if __name__ == "__main__":
     # тесты
-    if False:
-        s = ""
-        print(f"{s}->{is_int(s)}")
-
-        s = None
-        print(f"{s}->{is_int(s)}")
-
-        s = "1"
-        print(f"{s}->{is_int(s)}")
-
-        s = 2
-        print(f"{s}->{is_int(s)}")
-
-        s = "0.3"
-        print(f"{s}->{is_int(s)}")
-
-        s = "четыре"
-        print(f"{s}->{is_int(s)}")
 
     # тест быстродействия функции
     import time
-    def test(s, f, n):
+
+    n = 10000
+
+    def test(s, f):
         t = 0.0
         res = f(s)
         start_time = time.time()
@@ -41,22 +37,37 @@ if __name__ == "__main__":
         t += time.time() - start_time
         print("%5s->%s %.0f ms (%5s)" % (s, res, t*1000, f.__name__))
 
-    n = 10000
 
     if True:
-        test("", is_int, n)
-        test(None, is_int, n)
-        test("1", is_int, n)
-        test(2, is_int, n)
-        test("0.3", is_int, n)
-        test("f", is_int, n)
-        
+        test("", is_int)
+        test(None, is_int)
+        test("1", is_int)
+        test(2, is_int)
+        test("0.3", is_int)
+        test("f", is_int)
+
+        print('')
+
+        test("", is_int2)
+        test(None, is_int2)
+        test("1", is_int2)
+        test(2, is_int2)
+        test("0.3", is_int2)
+        test("f", is_int2)
+
         """
-             ->False 16 ms (is_int)
-         None->False 10 ms (is_int)
-            1->True 3 ms (is_int)
-            2->True 2 ms (is_int)
-          0.3->False 15 ms (is_int)
-            f->False 16 ms (is_int) 
+     ->False 18 ms (is_int)
+ None->False 12 ms (is_int)
+    1->True 3 ms (is_int)
+    2->True 2 ms (is_int)
+  0.3->False 20 ms (is_int)
+    f->False 19 ms (is_int)
+
+     ->False 18 ms (is_int2)
+ None->False 14 ms (is_int2)
+    1->True 3 ms (is_int2)
+    2->True 2 ms (is_int2)
+  0.3->False 17 ms (is_int2)
+    f->False 17 ms (is_int2)
         """
         
