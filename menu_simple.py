1e785bd32cec624a00580555c39ae114a8d95269
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:27:48 +0700

    Update menu_simple.py
    замена if на case
    код функции is_int(s) взят из is_int4 ветки is_int

--- menu_simple.py
+++ menu_simple.py
@@ -2,15 +2,72 @@ def is_int(s):
     """
     Проверка на то, что s - целое число
     """
-    return True
+    try:
+        if type(s) is int:
+            return True
+        if s is None:
+            return False
+        if not s.isdecimal():
+            return False
+        int(s)
+        return True
+    except (Exception, ValueError, TypeError):
+        return False
 
 
 def menu_sub1():
-    print("menu_sub1 \n")
+    """
+    Однократное выполнение действия и возврат в главное меню
+    """
+    ch = 0
+    # Ввод корректного ch
+    while True:
+        print("menu_sub1 \n 1) f1(x) \n 2) f2(x) \n 0) return \n")
+        ch = input()
+        if is_int(ch):
+            ch = int(ch)
+        if ch in [1, 2, 0]:
+            break
+        else:
+            print('err')
+    # Выбор действия
+    match ch:
+        case 1:
+            print('f1(x)')
+            input("pause", )
+        case 2:
+            print('f2(x)')
+            input("pause", )
+    # возврат в главное меню
 
 
 def menu_sub2():
-    print("menu_sub2 \n")
+    """
+    Циклическое. возврат в главное меню только через пункт 0
+    """
+    ch = 0
+    while True:
+        # Ввод корректного ch
+        while True:
+            print("menu_sub2 \n 1) g1(x) \n 2) g2(x) \n 0) return \n")
+            ch = input()
+            if is_int(ch):
+                ch = int(ch)
+            if ch in [1, 2, 0]:
+                break
+            else:
+                print('err')
+        # Выбор действия
+        match ch:
+            case 1:
+                print('g1(x)')
+                input("pause", )
+            case 2:
+                print('g2(x)')
+                input("pause", )
+            case 0:
+                break
+    # возврат в главное меню
 
 
 def menu_main():
@@ -22,15 +79,18 @@ def menu_main():
         ch = input()
         if is_int(ch):
             ch = int(ch)
-        if ch == 1:
-            menu_sub1()
-        elif ch == 2:
-            menu_sub2()
-        elif ch == 0:
-            break
-        else:
-            print('err')
+
+        match ch:
+            case 1:
+                menu_sub1()
+            case 2:
+                menu_sub2()
+            case 0:
+                break
+            case _:
+                print('err')
 
 
 if __name__ == "__main__":
     menu_main()
+    # exit(0)
