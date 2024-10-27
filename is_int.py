d7d02db75167a0f807b3310b4ef5d0b02c09678a
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:17:27 +0700

    Update is_int.py
    Добавлена проверка на None

--- is_int.py
+++ is_int.py
@@ -1,14 +1,3 @@
-def is_int2(s):
-    """
-    Проверка на то, что s - целое число
-    """
-    try:
-        int(s)
-        return True
-    except (Exception, ValueError, TypeError):
-        return False
-
-
 def is_int3(s):
     """
     Проверка на то, что s - целое число
@@ -16,6 +5,23 @@ def is_int3(s):
     try:
         if type(s) is int:
             return True
+        if not s.isdecimal():
+            return False
+        int(s)
+        return True
+    except (Exception, ValueError, TypeError):
+        return False
+
+
+def is_int4(s):
+    """
+    Проверка на то, что s - целое число
+    """
+    try:
+        if type(s) is int:
+            return True
+        if s is None:
+            return False
         if not s.isdecimal():
             return False
         int(s)
@@ -43,15 +49,6 @@ if __name__ == "__main__":
 
 
     if True:
-        test("", is_int2)
-        test(None, is_int2)
-        test("1", is_int2)
-        test(2, is_int2)
-        test("0.3", is_int2)
-        test("f", is_int2)
-
-        print('')
-        
         test("", is_int3)
         test(None, is_int3)
         test("1", is_int3)
@@ -59,18 +56,27 @@ if __name__ == "__main__":
         test("0.3", is_int3)
         test("f", is_int3)
 
-        """
-     ->False 16 ms (is_int2)
- None->False 12 ms (is_int2)
-    1->True 3 ms (is_int2)
-    2->True 2 ms (is_int2)
-  0.3->False 17 ms (is_int2)
-    f->False 16 ms (is_int2)
+        print('')
 
+        test("", is_int4)
+        test(None, is_int4)
+        test("1", is_int4)
+        test(2, is_int4)
+        test("0.3", is_int4)
+        test("f", is_int4)
+
+        """
      ->False 2 ms (is_int3)
- None->False 10 ms (is_int3)
-    1->True 4 ms (is_int3)
+ None->False 14 ms (is_int3)
+    1->True 5 ms (is_int3)
     2->True 1 ms (is_int3)
-  0.3->False 1 ms (is_int3)
+  0.3->False 2 ms (is_int3)
     f->False 2 ms (is_int3)
+
+     ->False 2 ms (is_int4)
+ None->False 2 ms (is_int4)
+    1->True 4 ms (is_int4)
+    2->True 2 ms (is_int4)
+  0.3->False 2 ms (is_int4)
+    f->False 2 ms (is_int4)
         """
