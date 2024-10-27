825a64a489c65e059b817994e204d9ed65ed6c9e
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:02:18 +0700

    Update is_int.py
    тест быстродействия функции is_int
    обнаружены проблемы

--- is_int.py
+++ is_int.py
@@ -11,7 +11,7 @@ def is_int(s):
 
 if __name__ == "__main__":
     # тесты
-    if True:
+    if False:
         s = ""
         print(f"{s}->{is_int(s)}")
 
@@ -29,3 +29,34 @@ if __name__ == "__main__":
 
         s = "четыре"
         print(f"{s}->{is_int(s)}")
+
+    # тест быстродействия функции
+    import time
+    def test(s, f, n):
+        t = 0.0
+        res = f(s)
+        start_time = time.time()
+        for i in range(n):
+            f(s)
+        t += time.time() - start_time
+        print("%5s->%s %.0f ms (%5s)" % (s, res, t*1000, f.__name__))
+
+    n = 10000
+
+    if True:
+        test("", is_int, n)
+        test(None, is_int, n)
+        test("1", is_int, n)
+        test(2, is_int, n)
+        test("0.3", is_int, n)
+        test("f", is_int, n)
+        
+        """
+             ->False 16 ms (is_int)
+         None->False 10 ms (is_int)
+            1->True 3 ms (is_int)
+            2->True 2 ms (is_int)
+          0.3->False 15 ms (is_int)
+            f->False 16 ms (is_int) 
+        """
+        
