950da72d7e90c9147cbeff2a66e5ce666fd7bced
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-27 23:32:32 +0700

    Update menu_simple.py
    Повторяющиеся код для ввода корректного пункта меню вынесен в функцию entering_valid_value_integer.

--- menu_simple.py
+++ menu_simple.py
@@ -15,21 +15,34 @@ def is_int(s):
         return False
 
 
+def entering_valid_value_integer(message_input: str, message_err: str, template: list):
+    """
+    Ввод допустимого целого числа
+    :param message_input: Сообщение перед вводом
+    :param message_err:   Сообщение при вводе символа, не из шаблона
+    :param template:      Список допустимых значений
+    :return: целое число
+    """
+    while True:
+        ch = input(message_input, )
+        if is_int(ch):
+            ch = int(ch)
+            if ch in template:
+                return ch
+        print(message_err)
+
+
 def menu_sub1():
     """
     Однократное выполнение действия и возврат в главное меню
     """
-    ch = 0
     # Ввод корректного ch
-    while True:
-        print("menu_sub1 \n 1) f1(x) \n 2) f2(x) \n 0) return \n")
-        ch = input()
-        if is_int(ch):
-            ch = int(ch)
-        if ch in [1, 2, 0]:
-            break
-        else:
-            print('err')
+    caption_start = "menu_sub1 \n 1) f1(x) \n 2) f2(x) \n 0) return \n"
+    caption_err = 'err'
+    template = [1, 2, 0]
+    ch = entering_valid_value_integer(caption_start,
+                                      caption_err,
+                                      template)
     # Выбор действия
     match ch:
         case 1:
@@ -45,18 +58,16 @@ def menu_sub2():
     """
     Циклическое. возврат в главное меню только через пункт 0
     """
-    ch = 0
+
+    caption_start = "menu_sub2 \n 1) g1(x) \n 2) g2(x) \n 0) return \n"
+    caption_err = 'err'
+    template = [1, 2, 0]
+
     while True:
         # Ввод корректного ch
-        while True:
-            print("menu_sub2 \n 1) g1(x) \n 2) g2(x) \n 0) return \n")
-            ch = input()
-            if is_int(ch):
-                ch = int(ch)
-            if ch in [1, 2, 0]:
-                break
-            else:
-                print('err')
+        ch = entering_valid_value_integer(caption_start,
+                                          caption_err,
+                                          template)
         # Выбор действия
         match ch:
             case 1:
@@ -74,12 +85,15 @@ def menu_main():
     """
     Циклическое. Выход из меню только через пункт 0
     """
+    caption_start = "menu_main \n 1) menu_sub1 \n 2) menu_sub2 \n 0) exit \n"
+    caption_err = 'err'
+    template = [1, 2, 0]
     while True:
-        print("menu_main \n 1) menu_sub1 \n 2) menu_sub2 \n 0) exit \n")
-        ch = input()
-        if is_int(ch):
-            ch = int(ch)
-
+        # Ввод корректного ch
+        ch = entering_valid_value_integer(caption_start,
+                                          caption_err,
+                                          template)
+        # Выбор действия
         match ch:
             case 1:
                 menu_sub1()
@@ -87,8 +101,7 @@ def menu_main():
                 menu_sub2()
             case 0:
                 break
-            case _:
-                print('err')
+    # завершение программы
 
 
 if __name__ == "__main__":
