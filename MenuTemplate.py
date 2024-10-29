9be55604754941c61bf98e3d535b075d37ccb03f
alekseyd11111 <50264921+alekseyd11111@users.noreply.github.com>
2024-10-29 23:47:06 +0700

    Разработан класс для создания меню.

--- MenuTemplate.py
+++ MenuTemplate.py
@@ -3,10 +3,6 @@ class MenuTemplate:
     Класс для создания меню
     """
 
-    # 1)
-    # menu = Menu_template(start_message="menu_main",
-    #                      key_separator=")"
-    #                      )
     def __init__(self,
                  start_message="",
                  key_separator=".",
@@ -18,21 +14,23 @@ class MenuTemplate:
         # пункты меню
         self.items = {}
 
-    # 2)
-    # menu.add_item("2", "f2(x)", lambda: print("f2"))
-    # menu.add_item("0", "exit", is_exit=True)
+    # 5)
+    # menu.add_item("1", "f11(x)", lambda: print("f11"))
+    # menu.add_item("1", "f1(x)", lambda: print("f1"))
     def add_item(self,
-                key: str,
-                message: str,
-                command=None,
-                is_exit=False):
+                 key: str,
+                 message: str,
+                 command=None,
+                 is_exit=False):
         """
         Добавление пункта меню
-        :param key:     уникальный символ пуекта меню.
-        :param message: Текст пункьа меню
+        :param key:     уникальный символ пункта меню
+        :param message: текст пункта меню
         :param command: функция, которая вызовиться
         :param is_exit: выход из меню после выполненения этого пункта
         """
+        if key in self.items:
+            raise Exception(f'the key "{key}" already exists')
 
         if command is None:
             def _pass():
@@ -41,20 +39,25 @@ class MenuTemplate:
 
         self.items[key] = {'message': message, 'command': command, 'is_exit': is_exit}
 
-    # 3)
-    # menu.start()
+    # 6)
     def start(self):
         """
-        Запуск вечного цикла меню.
+        Запуск вечного цикла меню
         """
         is_exit = False
         while not is_exit:
             # печать меню
             self.show()
-            is_exit = True
+            # Ввод пункта меню
+            key = input()
+            if key in self.items:
+                # Выбор действия
+                self.items[key]['command']()
+                # выход из меню
+                is_exit = self.items[key]['is_exit']
+            else:
+                print(self.message_invalid_key)
 
-    # 4)
-    # self.show()
     def show(self):
         """
         Вывод в консоль названия меню и его пунктов
@@ -69,11 +72,11 @@ if __name__ == "__main__":
     menu = MenuTemplate(start_message="menu_main",
                         key_separator=")"
                         )
-    menu.add_item("1", "f11(x)", lambda: print("f11"))
+    #menu.add_item("1", "f11(x)", lambda: print("f11"))
     menu.add_item("1", "f1(x)", lambda: print("f1"))
     menu.add_item("2", "f2(x)", lambda: print("f2"))
     menu.add_item("0", "exit", is_exit=True)
     menu.start()
 
-    menu.items["2"]['command']()
+    #menu.items["2"]['command']()
     exit(0)
